b679b0cd9ddb883f70ab05c31e6affe99ac7f89b
TenJit <tenjitrattanakul@gmail.com>
2024-11-29 19:42:57 +0700

    connect mongodb

--- main.py
+++ main.py
@@ -4,9 +4,28 @@ import cv2
 import numpy as np
 import base64
 from deepface import DeepFace
+from pymongo import MongoClient
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
+from bson.objectid import ObjectId
+from datetime import datetime
+import os
+from dotenv import load_dotenv
 
 app = FastAPI()
 
+try:
+    mongodb_uri = os.getenv("MONGODB_URI")    
+    print(f"Connecting to MongoDB using URI: {mongodb_uri}")
+    client = MongoClient(mongodb_uri)
+    client.admin.command('ping')  # Test the connection
+    print("Successfully connected to MongoDB! & connection strings = ",mongodb_uri)
+except Exception as e:
+    print(f"Error occurred while connecting to MongoDB: {e}")
+    raise
+
+db = client["emotion_detection"]  # Database name
+collection = db["emotions"]  # Collection name
 class ImageData(BaseModel):
     image: str 
     
@@ -22,7 +41,16 @@ async def detect_emotion(data: ImageData):
         img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
         result = DeepFace.analyze(img, actions=['emotion'])
         emotion = result[0]['dominant_emotion']
-        return {"emotion": emotion}
+        
+        emotion_data = {
+            "_id": ObjectId(),
+            "emotion": emotion,
+            "image_data": image_data,
+            "date_time" : datetime.now()
+        }
+        inserted_id = collection.insert_one(emotion_data).inserted_id
+        
+        return {"emotion": emotion , "object_id": str(inserted_id)}
 
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
