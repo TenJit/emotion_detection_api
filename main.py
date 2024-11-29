692733a04426f633b13cf127e59d799610981c6c
TenJit <tenjitrattanakul@gmail.com>
2024-11-29 23:27:30 +0700

    add get water and fix post

--- main.py
+++ main.py
@@ -6,15 +6,15 @@ import base64
 from deepface import DeepFace
 from pymongo import MongoClient
 from pymongo.mongo_client import MongoClient
-from pymongo.server_api import ServerApi
 from bson.objectid import ObjectId
-from datetime import datetime
+from datetime import datetime , timedelta
 import os
 from dotenv import load_dotenv
 
 app = FastAPI()
 
 try:
+    load_dotenv()
     mongodb_uri = os.getenv("MONGODB_URI")    
     print(f"Connecting to MongoDB using URI: {mongodb_uri}")
     client = MongoClient(mongodb_uri)
@@ -25,7 +25,8 @@ except Exception as e:
     raise
 
 db = client["emotion_detection"]  # Database name
-collection = db["emotions"]  # Collection name
+emotions_collection = db["emotions"]  # Collection name
+water_collection = db["water"]
 class ImageData(BaseModel):
     image: str 
     
@@ -48,9 +49,61 @@ async def detect_emotion(data: ImageData):
             "image_data": image_data,
             "date_time" : datetime.now()
         }
-        inserted_id = collection.insert_one(emotion_data).inserted_id
+        inserted_id = emotions_collection.insert_one(emotion_data).inserted_id
         
         return {"emotion": emotion , "object_id": str(inserted_id)}
 
+    except Exception as e:
+        if str(e) == "Face could not be detected in numpy array.Please confirm that the picture is a face photo or consider to set enforce_detection param to False.":
+            return {"emotion" : "No face detected"}
+        raise HTTPException(status_code=500, detail=str(e))
+
+@app.get("/water")
+async def get_water_data():
+    try:
+        current_date = datetime.now().strftime("%Y-%m-%d")
+        current_time = datetime.now().strftime("%H:%M:%S")
+        
+        water_data = water_collection.find_one({"date": current_date})
+        
+        if not water_data:
+            water_data = {
+                "date": current_date,
+                "water_time": []
+            }
+            water_collection.insert_one(water_data)
+            water_data = water_collection.find_one({"date": current_date})
+        
+        count_happy_emotion = emotions_collection.count_documents({
+            "emotion": "happy",
+            "date_time": {
+                "$gte": datetime.strptime(current_date, "%Y-%m-%d"), 
+                "$lt": datetime.strptime(current_date, "%Y-%m-%d") + timedelta(days=1)
+            }
+        })
+        
+        if len(water_data["water_time"]) < count_happy_emotion:
+            if len(water_data["water_time"]) >= 2:
+                return {
+                    "result": False,
+                    "water_time": water_data["water_time"]
+                }
+            else:   
+                water_collection.update_one(
+                    {"_id": water_data["_id"]},
+                    {
+                        "$push": {"water_time": {"time": current_time}}
+                    }
+                )
+                water_data = water_collection.find_one({"date": current_date})
+                return{
+                    "result": True,
+                    "water_time": water_data["water_time"]
+                }
+        else:
+            return {
+                "result": False,
+                "water_time": water_data["water_time"]
+            }
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
